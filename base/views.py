1a35ec557edb9b526b9a502c360051e79242cce8
parkerJenkins36 <pjenki17@uncc.edu>
2025-02-22 12:40:26 -0800

    User Profile Page

--- base/views.py
+++ base/views.py
@@ -85,6 +85,14 @@ def room(request, pk):
     context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    room = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'room':room, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/room.html', context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
