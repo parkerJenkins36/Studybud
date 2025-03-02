ee4a9179f0e119f1659456b7fd5e454bb6c6dc19
parkerJenkins36 <pjenki17@uncc.edu>
2025-03-01 18:58:36 -0800

    Edit User Account Page

--- base/urls.py
+++ base/urls.py
@@ -7,12 +7,13 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
 
-     path('', views.home, name="home"),
-     path('room/<str:pk>/', views.room, name="room"),
-     path('profile/<st:pk>/', views.userProfile, name="user-profile"),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<st:pk>/', views.userProfile, name="user-profile"),
 
-     path('create-room/', views.createRoom, name="create-room"),
-     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
-      path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
-      path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+    path('update-user/', views.updateUser, name="update-user"),
 ]
